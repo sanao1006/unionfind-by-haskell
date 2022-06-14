4ea9ea3f60413ff216f833be3912f84d208df77d
sanao <62137820+sanao1006@users.noreply.github.com>
2022-06-14 21:02:06 +0900

    Update 軽量union.hs

--- 軽量union.hs
+++ 軽量union.hs
@@ -59,77 +59,22 @@ uniteUF !uf !x !y = do
                             MA.writeArray (parent uf) a b
                             modifyArray(rank uf) b (+1)
 
+groupUF :: UnionFind -> IO Int
+groupUF uf = do
+  b <- MA.freeze(parent uf)::IO(UArray Int Int)
+  groupUF' (A.elems b) 0
+  where
+    groupUF':: [Int] -> Int ->IO Int
+    groupUF' [] ans = return ans
+    groupUF' (a:as) ans
+      | a < 0 = groupUF' as (ans+1)
+      | otherwise = groupUF' as ans
+
 modifyArray :: (MA.MArray a e m, Ix i) => a i e -> i -> (e -> e) -> m ()
 {-# INLINE modifyArray #-}
 modifyArray !a !i !f = do
   !ai <- MA.readArray a i
   MA.writeArray a i (f ai)
 
-main :: IO ()
-main = do
-    [!n, !q] <- input @[Int]
-    !tuv <- inputs @[[Int]]
-    !uf <- initUF n
-    forM_ tuv $ \[!t,!u,!v] -> do
-        !same <- sameUF uf u v
-        case t of
-          0 -> uniteUF uf u v
-          1 -> print $ if same then 1 else 0
     
-
--- ==================================================================
-
-type Bs = BS.ByteString
-type Height = Int
-type Width = Int
-type MutableArray2D any = IOUArray (Height,Width) any
-type Array2D any = UArray (Height, Width) any
-
--- 入力 =====================================================================================================================================================
-class (Read a) => Input a where
-  read' :: BS.ByteString -> a
-  readArray2D :: Height -> Width -> BS.ByteString -> a
-
-input :: Input a => IO a
-{-# INLINE input #-}
-input = read' <$!> BS.getLine
-
-inputs :: Input a => IO a
-{-# INLINE inputs #-}
-inputs = read' <$!> BS.getContents
-
-inputArray :: Input a => Int -> Int -> IO a
-{-# INLINE inputArray #-}
-inputArray = flip flip BS.getContents . ((<$>) .) . readArray2D
-
-instance Input Bs where
-  read' = id
-instance Input Int where
-  read' = fst . fromJust . BS.readInt
-instance Input Integer where
-  read' = (read @Integer) . BS.unpack
-instance Input Double where
-  read' = read . BS.unpack
-instance Input [Bs] where
-  read' = BS.words
-instance Input [Int] where
-  read' = map ((read @Int) . BS.unpack) . BS.words
-instance Input [Double] where
-  read' = map ((read @Double) . BS.unpack) . BS.words
-instance Input [(Int, Bs)] where
-  read' = map ((\[a, b] -> (fst (fromJust (BS.readInt a)), b)) . BS.words) . BS.lines
-instance Input [(Bs, Int)] where
-  read' = map ((\[a, b] -> (a, fst (fromJust (BS.readInt b)))) . BS.words) . BS.lines
-instance Input [[Bs]] where
-  read' = map BS.words . BS.lines
-instance Input [[Int]] where
-  read' = map (map (fst . fromJust . BS.readInt) . BS.words) . BS.lines
-instance Input [[Double]] where
-  read' = map (map ( (read @Double) . BS.unpack) . BS.words) . BS.lines
-instance Input (Array2D Int) where
-  readArray2D = flip flip ((map (fst . fromJust . BS.readInt) . BS.words) <=< BS.lines) . (((.) . A.listArray . ((0, 0) ,)) .) . (. subtract 1) . (,) . subtract 1
-instance Input (Array2D Double) where
-  readArray2D height width = A.listArray ((0, 0), (height - 1, width - 1)) . concatMap (map ((read @Double) . BS.unpack) . BS.words) . BS.lines
-instance Input (Array2D Char) where
- readArray2D = flip flip (BS.unpack <=< BS.lines) . (((.) . A.listArray . ((0, 0) ,)) .) . (. subtract 1) . (,) . subtract 1
--- ============================================================================================================================================================
+ 
